f516446ac02bc67ab9f574b05a271b27fff4d029
EvgenyGuguev <evgenygost@gmail.com>
2021-01-25 21:38:45 +0300

    best practice for configs

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,14 +6,16 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { CoffeeRatingModule } from './coffee-rating/coffee-rating.module';
 import { ConfigModule } from '@nestjs/config';
 import * as Joi from '@hapi/joi';
+import appConfig from './config/app.config';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
-      validationSchema: Joi.object({
-        DATABASE_HOST: Joi.required(),
-        DATABASE_PORT: Joi.number().default(5432),
-      }),
+      // validationSchema: Joi.object({
+      //   DATABASE_HOST: Joi.required(),
+      //   DATABASE_PORT: Joi.number().default(5432),
+      // }),
+      load: [appConfig], // another variant of env configs
     }),
     CoffeesModule,
     TypeOrmModule.forRoot({
